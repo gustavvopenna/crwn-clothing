e83887dc04aa215d8174b1edb39f5b0f602940d8
gustavvopenna <salvador@la-MacBook-Air-de-Gustavo.local>
2019-10-28 15:37:41 -0600

    Add hover effect

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,18 +2,43 @@ import React from 'react'
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}
-  >
-    <div className='content'>
-      <h1 className='title'>{title}</h1>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
-)
+class MenuItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isHovering: false
+    }
+  }
+
+  handleMouse = () => {
+    this.setState((prevState, props) => ({
+      isHovering: !prevState.isHovering
+    }))
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          backgroundImage: `url(${this.props.imageUrl})`
+        }}
+        className={`${this.props.size} menu-item`}
+      >
+        <div
+          className='content'
+          onMouseEnter={this.handleMouse}
+          onMouseLeave={this.handleMouse}
+        >
+          {this.state.isHovering && (
+            <div>
+              <h1 className='title'>{this.props.title}</h1>
+              <span className='subtitle'>SHOP NOW</span>
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default MenuItem
