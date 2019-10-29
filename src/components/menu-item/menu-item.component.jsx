6cf1fa87e22f4514024bacd4b4979087c89555b9
gustavvopenna <salvador@la-MacBook-Air-de-Gustavo.local>
2019-10-28 22:56:46 -0600

    Style menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,43 +2,21 @@ import React from 'react'
 
 import './menu-item.styles.scss'
 
-class MenuItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isHovering: false
-    }
-  }
-
-  handleMouse = () => {
-    this.setState((prevState, props) => ({
-      isHovering: !prevState.isHovering
-    }))
-  }
-
-  render() {
-    return (
-      <div
-        style={{
-          backgroundImage: `url(${this.props.imageUrl})`
-        }}
-        className={`${this.props.size} menu-item`}
-      >
-        <div
-          className='content'
-          onMouseEnter={this.handleMouse}
-          onMouseLeave={this.handleMouse}
-        >
-          {this.state.isHovering && (
-            <div>
-              <h1 className='title'>{this.props.title}</h1>
-              <span className='subtitle'>SHOP NOW</span>
-            </div>
-          )}
-        </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className='background-image'
+    />
+    <div className='content'>
+      <div>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+)
 
 export default MenuItem
